fa7e152f2cb81158ae1bba6faa97ed6c097f4312
Urveel <urveelp@gmail.com>
2023-11-28 11:26:42 +0000

    Urveel patel

---
+++ src/testsuite/LoginTest.java
@@ -0,0 +1,48 @@
+package testsuite;
+
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import utilities.Utility;
+
+import java.util.List;
+
+public class LoginTest extends Utility {
+    String baseUrl = " https://www.saucedemo.com/";
+
+    @Before
+    public void browsersetup() {
+        OpenBrowser(baseUrl);
+    }
+
+    @Test
+    public void userShouldLoginSuccessfullyWithValidCredentials() {
+        sentTextToElement(By.id("user-name"),"standard_user");
+        sentTextToElement(By.id("password"),"secret_sauce");
+        clickOnElement(By.id("login-button"));
+
+        String actual = getTextFromElement(By.xpath("//span[contains(text(),'Products')]"));
+        String expected = "Products";
+        Assert.assertEquals(expected,actual);
+
+    }
+
+    @Test
+    public void verifyThatSixProductsAreDisplayedOnPage(){
+
+        sentTextToElement(By.id("user-name"),"standard_user");
+        sentTextToElement(By.id("password"),"secret_sauce");
+        clickOnElement(By.id("login-button"));
+        List<WebElement> items = driver.findElements(By.xpath("//div[@class = 'inventory_list']//div[@class = 'inventory_item']"));
+        System.out.println("Total Products Displayed on Page : " + items.size());
+    }
+
+    @After
+    public void tearDown() {
+
+        closeBrowser();
+    }
+}
